b34711c48fe43ca04cf2f72b1f5d0ee8433db029
NotVndtta <thenotalexey192@gmail.com>
2023-07-20 00:29:51 +0300

    add shuffle function and images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+const cardImages = [
+  {"src": "/img/pr1.png"},
+  {"src": "/img/pr2.png"},
+  {"src": "/img/pr3.png"},
+  {"src": "/img/pr4.png"},
+  {"src": "/img/pr5.png"},
+  {"src": "/img/pr6.png"},
+]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0,5)
+    .map((card) => ({...card, id:Math.random() }))
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+  console.log(cards,turns)
   return (
     <div className="App">
       <h1>Memory game</h1>
-      <button> Start Game</button>
+      <button onClick={shuffleCards}> Start Game</button>
     </div>
   );
 }
